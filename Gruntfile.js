c0f82f300681391cfb1fe2f14b0a3bdefa0cd30d
Sigfried Gold <sgold@s-3.com>
2013-12-23 08:51:28 -0500

    trying to get this working as a bower package

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      './dist/supergroup.js': [
+        './supergroup'
+        ,'./bower_components/underscore-unchained/src/underscore-unchained.js'
+        ,'./bower_components/underscore/underscore-min.js'
+        ,'./bower_components/1670507/underscoreAddon.js'
+      ]
+      , options: { 
+            debug: true
+          , s: true 
+          , transform: ['debowerify', 'decomponentify', 'deamdify', 'deglobalify'],
+      }
+    },
+    watch: {
+      files: [ './supergroup.js',"./README.md"],
+      tasks: [ 'browserify' ]
+    },
+    groc: {
+        javascript: [
+            "./supergroup.js", "README.md"
+        ],
+        options: {
+        "out": "doc/"
+        }
+    },
+    jshint: {
+        all: ['Gruntfile.js', './*.js', 'test/**/*.js']
+        , options: { laxcomma: true }
+        //,environments: ['node']
+    }
+  });
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-groc');
+  grunt.registerTask('default', ['browserify', 'jshint', 'groc']);
+};
